2da94c5985da061ad8ef340c0a11a0747e445ba2
Thomas Malt <thomas@malt.no>
2021-07-01 16:18:23 +0200

    updated menu

--- 2021/KnowitMenu.ts
+++ 2021/KnowitMenu.ts
@@ -2,11 +2,13 @@
  * Adds a custum menu to the document
  */
 function onOpen() {
-    const sheet = SpreadsheetApp.getActiveSpreadsheet();
-    const entries = [
-        {
-            name: "Update Competency Data", functionName: "updateCompetencyData",
-        }
-    ];
-    sheet.addMenu("knowit.no", entries);
+  const sheet = SpreadsheetApp.getActiveSpreadsheet();
+  const entries = [
+    {
+      name: 'Update Competency Data',
+      functionName: 'updateCompetencyData',
+    },
+    { name: 'Generate Data Sheet', functionName: 'generateDataSheet' },
+  ];
+  sheet.addMenu('knowit.no', entries);
 }
