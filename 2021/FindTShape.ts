8f9894fe6615913a3de086d5c7e480f6179be76e
Thomas Malt <thomas@malt.no>
2022-06-07 20:31:09 +0200

    fixed calculating competency group

--- 2021/FindTShape.ts
+++ 2021/FindTShape.ts
@@ -1,21 +1,24 @@
 /**
- * Calculates the primary, secondary and tertiary competency for a user
+ * Calculates the primary, secondary and tertiary competency area for a user
  *
  * @returns Array
  * @customfunction
  */
 function calcTShape(data: any[][]) {
   const sh = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('T-shape');
+
   if (sh === null) throw new TypeError('Sheet T-shape was null');
 
-  const ra = sh.getRange(2, 3, 1, 9);
-  const colors = ra.getBackgrounds();
-  console.log('color', colors);
+  const ra = sh.getRange(2, 4, 1, 9);
   const categories = ra.getValues().flat();
+
+  console.log('categories', categories);
+
   const output = data.map((r) => {
     return r
       .map((e, i) => [e, i])
-      .sort((a, b) => (a[0] < b[0] ? 1 : -1))
+      .filter((e) => e[0] >= 1.0)
+      .sort((a, b) => b[0] - a[0])
       .map((e) => categories[e[1]].substr(0, 12))
       .slice(0, 3);
   });
