90c3c1047581868a600ee1d0e44ef9e521bcb599
Thomas Malt <thomas@malt.no>
2022-06-07 22:23:47 +0200

    implemented blacklist of users that have quit

--- 2021/FetchAllData.ts
+++ 2021/FetchAllData.ts
@@ -1,60 +1,10 @@
 /**
- * Type definitions
- */
-interface Question {
-  index: number;
-  type: string;
-  categoryID: string;
-  text: string; // 'Relasjonsdatabaser som Postgres, Oracle o.l.',
-  topic: string; // topic: 'Relasjonsdatabaser',
-  id: string; // id: 'b1656d08-1f76-443e-a23f-b6179235da75' } },
-}
-
-interface Answer {
-  knowledge?: number; // knowledge: 3,
-  motivation?: number; //     motivation: 2,
-  customScaleValue?: number;
-  updatedAt: string; //     updatedAt: '2021-02-22T12:15:51.688Z',
-  question: Question; //     question:
-}
-
-interface TaxonomyTree {
-  [key: string]: string;
-}
-
-interface Category {
-  index: number;
-  text: string;
-  description: string;
-  id: string;
-}
-
-interface UserAnswers {
-  username: string;
-  email: string;
-  formDefinitionID: string;
-  updatedAt: string;
-  answers: Answer[];
-}
-
-interface UserAttribute {
-  Name: string;
-  Value: any;
-}
-interface User {
-  username: string;
-  attributes: UserAttribute[];
-}
-
-/**
  * Wrapper for the ugly UrlFetchApp.fetch function
  *
  * @param url string - the URL to fetch
  * @returns any - the object return from JSON.parse
  */
 function _fetch(url: string): any {
-  console.log('fetching url:', url);
-
   const res = UrlFetchApp.fetch(url, {
     headers: {
       'x-api-key': config.apikey,
@@ -65,13 +15,19 @@ function _fetch(url: string): any {
     console.log(`status: ${status}. Aborting update.`);
     return;
   }
-  console.log(`status: ${status}. Continuing`);
 
   return JSON.parse(res.getContentText());
 }
 
 function transpose(a: any[][]): any[][] {
   return a[0].map((_, i) => a.map((x) => x[i]));
+}
+
+function getUserBlacklist(): string[] {
+  const sBlacklist = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('user blacklist');
+  if (sBlacklist === null) throw new TypeError('Spreadsheet sheet user blacklist is null');
+
+  return sBlacklist.getDataRange().getValues().flat().slice(2);
 }
 
 /**
@@ -87,10 +43,8 @@ function generateDataSheet() {
   sData.clearContents();
   sNotAnswered.clearContents();
 
-  const users = getUserList().map((u) => u[0]);
+  const users = getUserList();
   const categories: Category[] = getCategoriesData();
-
-  console.log('categories', categories);
 
   let catMap = new Map();
 
@@ -98,7 +52,6 @@ function generateDataSheet() {
     catMap.set(e.id, e.text);
   });
 
-  // console.log('catmap', catMap.keys());
   const allQuestions = getQuestions();
   const compQuestions = allQuestions
     .filter((a) => a[3] === 'knowledgeMotivation')
@@ -118,9 +71,8 @@ function generateDataSheet() {
       return [e[6], e[1], e[4]];
     });
 
-  console.log('job questions:', jobQuestions);
   const questions = jobQuestions.concat(compQuestions, compQuestions);
-
+  const blacklist = getUserBlacklist();
   const all = getAllAnswersData()
     .map((u: UserAnswers) => {
       let r = [u.email, u.username, u.updatedAt.slice(0, 10)];
@@ -174,31 +126,31 @@ function generateDataSheet() {
       });
       return r;
     })
-    .sort((a, b) => (a[0] > b[0] ? 1 : -1)); // Sort by email
+    .sort((a, b) => (a[0] > b[0] ? 1 : -1)) // Sort by email
+    .filter((u) => !blacklist.includes(u[0])); // Remove blacklisted users
 
+  // figure out who has not answered
   const answered = all.map((u) => u[0]);
   const notAnswered = users.filter((u) => !answered.includes(u)).map((u) => [u]);
+
+  // transpose questions to print horizontally
   const transposed = transpose(questions);
-  const updated = `Last updated: ${new Date().toLocaleString('se')}`;
 
   sData.getRange(4, 1, 1, 3).setValues([['email', 'user id', 'updated at']]);
   sData.getRange(2, 4, transposed.length, transposed[0].length).setValues(transposed);
   sData.getRange(5, 1, all.length, all[0].length).setValues(all);
   sData.getRange(1, 6).setValue('Kompetanse');
   sData.getRange(1, 157).setValue('Motivasjon');
+
+  // set last updated string
+  const updated = `Last updated: ${new Date().toLocaleString('se')}`;
   sData.getRange(1, 1).setValue(updated);
+
   if (notAnswered.length > 0) {
     sNotAnswered.getRange(3, 1, notAnswered.length, 1).setValues(notAnswered);
     sNotAnswered.getRange(2, 1).setValue('email');
     sNotAnswered.getRange(1, 1).setValue(updated);
   }
-}
-
-function getUserListData(): User[] {
-  const data = _fetch(config.urls.users);
-  console.log('userlist', data.slice(0, 3));
-  console.log(data[1]);
-  return data;
 }
 
 /**
@@ -207,11 +159,12 @@ function getUserListData(): User[] {
  * @returns any[]
  */
 function getUserList(): any[] {
-  const data = getUserListData();
+  const data = _fetch(config.urls.users);
   const output = data
-    .map((user: User) => [user.attributes[0].Value, user.username])
-    .filter((e) => e[0] !== 'user@user.user')
-    .sort((a, b) => (a[0] > b[0] ? 1 : -1));
+    .map((user: User) => <string[]>[user.attributes[0].Value, user.username])
+    .map((e: string[]) => e[0])
+    .filter((e: string) => e !== 'user@user.user')
+    .sort();
 
   return output;
 }
@@ -229,8 +182,6 @@ function getAnswersForUsername(username: string, type: KnowledgeMotivation) {
   const data = _fetch(`${config.urls.answers}/${username}/newest`);
   const questions: Question[] = _fetch(`${config.urls.catalogs}/${config.catalogs.latest}/questions`);
   const qlist = questions.map((q) => q.id).sort();
-
-  // console.log(qlist);
 
   const answers = qlist.map((id) => {
     const found = data.answers.find((a: Answer) => id === a.question.id);
@@ -276,7 +227,6 @@ function getCategoriesData(): Category[] {
 
 function getAllAnswersData(): UserAnswers[] {
   const data = _fetch(config.urls.answers);
-  console.log('all answers', data);
   return data;
 }
 
