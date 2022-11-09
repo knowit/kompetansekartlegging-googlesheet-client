2bae860347096ab2e2a0a7cd11f0990792589665
Thomas Malt <thomas@malt.no>
2022-11-09 11:43:22 +0100

    getting ready for 2022

--- 2022/KnowitMenu.ts
+++ 2022/KnowitMenu.ts
@@ -1,7 +1,10 @@
 /**
  * Adds a custum menu to the document
+ * Updated 2022-11-08
+ * @author <thomas.malt@knowit.no>
  */
 function onOpen() {
+  const update = "2022-11-08";
   const sheet = SpreadsheetApp.getActiveSpreadsheet();
   const entries = [
     {
